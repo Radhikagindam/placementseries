fcb03e519e1f4ddf8f892200020e0bae3a32d339
Radhikagindam <148328904+Radhikagindam@users.noreply.github.com>
2025-08-10 00:23:22 +0530

    Create palindrom.py

---
+++ palindrom.py
@@ -0,0 +1,8 @@
+n=1234
+num=n
+result=0
+while (num>0):
+    ld=num%10 #to extract the ex:4 
+    result=result*10+ld  # initial result is 0*10+4=4
+    num=num//10 #apart from extracted value ex: 123
+print(n==result) # palindrom comparision function 
